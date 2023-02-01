e200af09c26c6312e19877447ddfb64e415c527f
UJGunner <122560296+UJGunner@users.noreply.github.com>
2023-02-01 08:22:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,111 +1,137 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (1 == receivedNumber) {
-        if (1 == selections) {
-            Ties += 1
-            Score_Board()
-        } else if (2 == selections) {
-            Player_1_Points += 1
-            Score_Board()
-        } else {
-            Player_2_Points += 1
-            Score_Board()
+    if (receivedNumber == confirm) {
+        if (1 == receivedNumber) {
+            if (1 == selections) {
+                Ties += 1
+                Score_Board()
+            } else if (2 == selections) {
+                Wins += 1
+                Score_Board()
+            } else {
+                Loss += 1
+                Score_Board()
+            }
         }
-    }
-    if (2 == receivedNumber) {
-        if (1 == selections) {
-            Player_2_Points += 1
-            Score_Board()
-        } else if (2 == selections) {
-            Ties += 1
-            Score_Board()
-        } else {
-            Player_1_Points += 1
-            Score_Board()
+        if (2 == receivedNumber) {
+            if (1 == selections) {
+                Loss += 1
+                Score_Board()
+            } else if (2 == selections) {
+                Ties += 1
+                Score_Board()
+            } else {
+                Wins += 1
+                Score_Board()
+            }
         }
-    }
-    if (3 == receivedNumber) {
-        if (1 == selections) {
-            Player_1_Points += 1
-            Score_Board()
-        } else if (2 == selections) {
-            Player_2_Points += 1
-            Score_Board()
-        } else {
-            Ties += 1
-            Score_Board()
+        if (3 == receivedNumber) {
+            if (1 == selections) {
+                Wins += 1
+                Score_Board()
+            } else if (2 == selections) {
+                Loss += 1
+                Score_Board()
+            } else {
+                Ties += 1
+                Score_Board()
+            }
         }
     }
 })
 input.onButtonPressed(Button.A, function () {
-    selections += 1
+    if (0 == Connection) {
+        confirm += 1
+    } else {
+        selections += 1
+    }
 })
 function Score_Board () {
     Rounds += 1
     OLED.clear()
-    OLED.writeStringNewLine("P1 Points" + Player_1_Points)
+    OLED.writeStringNewLine("P1 Points" + Wins)
     OLED.newLine()
-    OLED.writeStringNewLine("P2 Points" + Player_2_Points)
+    OLED.writeStringNewLine("P2 Points" + Loss)
     OLED.newLine()
     OLED.writeStringNewLine("Ties" + Ties)
     OLED.newLine()
     OLED.writeStringNewLine("Rounds" + Rounds)
 }
 input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(selections)
+    if (0 == Connection) {
+        Connection = confirm
+    } else {
+        radio.sendNumber(Connection)
+        radio.sendNumber(selections)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    selections += -1
+    if (0 == Connection) {
+        confirm += -1
+    } else {
+        selections += -1
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     Reset()
 })
 function Reset () {
     OLED.init(128, 64)
-    Player_1_Points = 0
-    Player_2_Points = 0
+    Wins = 0
+    Loss = 0
     Ties = 0
     Rounds = 0
     selections = 0
     Score_Board()
 }
 let Rounds = 0
-let Player_2_Points = 0
-let Player_1_Points = 0
+let Loss = 0
+let Wins = 0
 let Ties = 0
 let selections = 0
+let confirm = 0
+let Connection = 0
+Connection = 0
+confirm = 0
 Reset()
 basic.forever(function () {
-    if (0 == selections) {
-        selections += 3
+    if (0 < Connection) {
+        if (0 == selections) {
+            selections += 3
+        }
+        if (1 == selections) {
+            basic.showLeds(`
+                # # # . .
+                # . . # .
+                # # # . .
+                # . . # .
+                # . . . #
+                `)
+        }
+        if (2 == selections) {
+            basic.showLeds(`
+                # # # . .
+                # . . # .
+                # # # . .
+                # . . . .
+                # . . . .
+                `)
+        }
+        if (3 == selections) {
+            basic.showLeds(`
+                . # # # .
+                # . . . .
+                . # # . .
+                . . . # .
+                # # # . .
+                `)
+        }
+        if (4 <= selections) {
+            selections += -3
+        }
     }
-    if (1 == selections) {
-        basic.showLeds(`
-            # # # . .
-            # . . # .
-            # # # . .
-            # . . # .
-            # . . . #
-            `)
-    }
-    if (2 == selections) {
-        basic.showLeds(`
-            # # # . .
-            # . . # .
-            # # # . .
-            # . . . .
-            # . . . .
-            `)
-    }
-    if (3 == selections) {
-        basic.showLeds(`
-            . # # # .
-            # . . . .
-            . # # . .
-            . . . # .
-            # # # . .
-            `)
-    }
-    if (4 <= selections) {
-        selections += -3
+})
+basic.forever(function () {
+    if (0 == Connection) {
+        basic.showString("" + (confirm))
     }
 })
